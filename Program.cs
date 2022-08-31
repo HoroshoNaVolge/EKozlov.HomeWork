34e429ee420ae6705f940d24ac4f970ceba6a3c0
Evgeny <evgenij.kozlov@gmail.com>
2022-08-31 12:35:28 +0300

    изменил структуру проекта. логика теперь в общем проект дз , и всего одно будет приложение на все дз

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿/*
-Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3
-*/
-
+﻿using MyLibrary;
 public class Program
 {
     private static void Main(string[] args)
@@ -44,24 +38,3 @@ public static class MyMethods{
     }
 }
  #endregion
-
-#region Класс с оберткой для консоли;
-public static class MyConsole // обёртка для возможности записи в одну строку запроса на ввод(WriteLine) и ReadLine 
-{
-    public static string ReadLine() => System.Console.ReadLine(); // возврат базового метода при отсутстии аргументов
-
-    public static string ReadLine(string message) // перегрузка с аргументом, который необходимо вывести при помощи WriteLine
-    {
-        System.Console.WriteLine(message);
-        return ReadLine();
-    }
-}
-#endregion
-
-#region Отделенные строки вывода в консоль
-public static class Message
-{
-    public static string incorrectInputMessage = "Некорректный ввод. Повторите попытку ввода целого числа: " ;
-    public static string inviteToInputNumberMessage = "Введите целое число: ";
-}
-#endregion
